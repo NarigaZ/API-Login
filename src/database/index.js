9890202d22f30d83ab5e7e8ea30c009d0c2f10db
Felipe Rezende <felipe.rezende@docnix.com.br>
2019-11-09 17:26:24 -0300

    API Login Start V1.0

---
+++ src/database/index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://narigaz:narigaz7@d4rks-j72np.mongodb.net/test?retryWrites=true&w=majority', { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+ });
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
